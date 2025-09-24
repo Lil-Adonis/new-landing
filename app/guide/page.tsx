65777c87079b93fa11c1df7b506ae4349e1f8114
Lil-Adonis <adenugaib24@gmail.com>
2025-09-25 00:51:46 +0100

    guide edit

--- app/guide/page.tsx
+++ app/guide/page.tsx
@@ -37,7 +37,7 @@ export default function PageServices2() {
 										</div>
 										<h5 className="my-3">Predictive Maintenance Strategies</h5>
 										<p className="mb-6">How to implement predictive maintenance to minimize downtime</p>
-										<Link href="#" className="text-primary fs-7 fw-bold">
+										{/* <Link href="#" className="text-primary fs-7 fw-bold">
 											Learn More
 											<svg className=" ms-2 " xmlns="http://www.w3.org/2000/svg" width={19} height={18} viewBox="0 0 19 18" fill="none">
 												<g clipPath="url(#clip0_399_9647)">
@@ -49,7 +49,7 @@ export default function PageServices2() {
 													</clipPath>
 												</defs>
 											</svg>
-										</Link>
+										</Link> */}
 									</div>
 								</div>
 							</div>
@@ -63,7 +63,7 @@ export default function PageServices2() {
 										</div>
 										<h5 className="my-3">Ai for smarter facility operations</h5>
 										<p className="mb-6">Leveraging artificial intelligence to optimize facility management.</p>
-										<Link href="#" className="text-primary fs-7 fw-bold">
+										{/* <Link href="#" className="text-primary fs-7 fw-bold">
 											Learn More
 											<svg className=" ms-2 " xmlns="http://www.w3.org/2000/svg" width={19} height={18} viewBox="0 0 19 18" fill="none">
 												<g clipPath="url(#clip0_399_9647)">
@@ -75,7 +75,7 @@ export default function PageServices2() {
 													</clipPath>
 												</defs>
 											</svg>
-										</Link>
+										</Link> */}
 									</div>
 								</div>
 							</div>
@@ -89,7 +89,7 @@ export default function PageServices2() {
 										</div>
 										<h5 className="my-3">Achieving Compliance in Healthcare</h5>
 										<p className="mb-6">A step-by-step guide to maintaining compliance in healthcare facilities.</p>
-										<Link href="#" className="text-primary fs-7 fw-bold">
+										{/* <Link href="#" className="text-primary fs-7 fw-bold">
 											Learn More
 											<svg className=" ms-2 " xmlns="http://www.w3.org/2000/svg" width={19} height={18} viewBox="0 0 19 18" fill="none">
 												<g clipPath="url(#clip0_399_9647)">
@@ -101,7 +101,7 @@ export default function PageServices2() {
 													</clipPath>
 												</defs>
 											</svg>
-										</Link>
+										</Link> */}
 									</div>
 								</div>
 							</div>
@@ -115,7 +115,7 @@ export default function PageServices2() {
 										</div>
 										<h5 className="my-3">Vendor Management Best Practices</h5>
 										<p className="mb-6">Optimize vendor relationships and streamline operations.</p>
-										<Link href="#" className="text-primary fs-7 fw-bold">
+										{/* <Link href="#" className="text-primary fs-7 fw-bold">
 											Learn More
 											<svg className=" ms-2 " xmlns="http://www.w3.org/2000/svg" width={19} height={18} viewBox="0 0 19 18" fill="none">
 												<g clipPath="url(#clip0_399_9647)">
@@ -127,7 +127,7 @@ export default function PageServices2() {
 													</clipPath>
 												</defs>
 											</svg>
-										</Link>
+										</Link> */}
 									</div>
 								</div>
 							</div>
@@ -141,7 +141,7 @@ export default function PageServices2() {
 										</div>
 										<h5 className="my-3">Towards Greener Facilities</h5>
 										<p className="mb-6">Sustainable Practices to reduce your facilities environmental impact.</p>
-										<Link href="#" className="text-primary fs-7 fw-bold">
+										{/* <Link href="#" className="text-primary fs-7 fw-bold">
 											Learn More
 											<svg className=" ms-2 " xmlns="http://www.w3.org/2000/svg" width={19} height={18} viewBox="0 0 19 18" fill="none">
 												<g clipPath="url(#clip0_399_9647)">
@@ -153,7 +153,7 @@ export default function PageServices2() {
 													</clipPath>
 												</defs>
 											</svg>
-										</Link>
+										</Link> */}
 									</div>
 								</div>
 							</div>
@@ -167,7 +167,7 @@ export default function PageServices2() {
 										</div>
 										<h5 className="my-3">Automation in Retail Environments</h5>
 										<p className="mb-6">How retail facilities can benefit from automated system.</p>
-										<Link href="#" className="text-primary fs-7 fw-bold">
+										{/* <Link href="#" className="text-primary fs-7 fw-bold">
 											Learn More
 											<svg className=" ms-2 " xmlns="http://www.w3.org/2000/svg" width={19} height={18} viewBox="0 0 19 18" fill="none">
 												<g clipPath="url(#clip0_399_9647)">
@@ -179,7 +179,7 @@ export default function PageServices2() {
 													</clipPath>
 												</defs>
 											</svg>
-										</Link>
+										</Link> */}
 									</div>
 								</div>
 							</div>
@@ -187,7 +187,7 @@ export default function PageServices2() {
 								<path className="fill-primary-soft" d="M0 130.481C0 110.236 15.1267 93.1822 35.2276 90.7667L783.228 0.880261C807.04 -1.98124 828 16.611 828 40.5945V533.155C828 552.691 813.888 569.369 794.622 572.603L46.6224 698.173C22.2271 702.269 0 683.462 0 658.725V130.481Z" fill="#F5EEFF" />
 							</svg>
 						</div>
-						<div className="text-center mt-6 d-flex flex-wrap justify-content-center align-items-center gap-3">
+						{/* <div className="text-center mt-6 d-flex flex-wrap justify-content-center align-items-center gap-3">
 							<Link href="/page-services-1" className="btn btn-gradient">
 								Explore Now
 								<svg className="ms-2" xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
@@ -195,13 +195,13 @@ export default function PageServices2() {
 									<path className="stroke-white" d="M17 7L6.75 17.25" stroke="white" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
 								</svg>
 							</Link>
-							<Link href="/page-contact-1" className="btn btn-outline-secondary hover-up">
+							<Link href="/contact" className="btn btn-outline-secondary hover-up">
 								<svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} viewBox="0 0 24 24" fill="none">
 									<path className="stroke-dark" d="M8.89286 4.75H6.06818C5.34017 4.75 4.75 5.34017 4.75 6.06818C4.75 13.3483 10.6517 19.25 17.9318 19.25C18.6598 19.25 19.25 18.6598 19.25 17.9318V15.1071L16.1429 13.0357L14.5317 14.6468C14.2519 14.9267 13.8337 15.0137 13.4821 14.8321C12.8858 14.524 11.9181 13.9452 10.9643 13.0357C9.98768 12.1045 9.41548 11.1011 9.12829 10.494C8.96734 10.1537 9.06052 9.76091 9.32669 9.49474L10.9643 7.85714L8.89286 4.75Z" stroke="#111827" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
 								</svg>
 								Contact Us
 							</Link>
-						</div>
+						</div> */}
 					</div>
 					<div className="position-absolute top-0 start-50 translate-middle-x z-0">
 						<img src="/assets/imgs/service-2/bg-line.png" alt="limpiar" />
