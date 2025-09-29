910340de21d681034220377f75d587da386a3806
Lil-Adonis <adenugaib24@gmail.com>
2025-09-29 11:33:06 +0100

    bg change

--- app/loading.tsx
+++ app/loading.tsx
@@ -25,7 +25,7 @@ export default function Preloader() {
 					left: 0;
 					right: 0;
 					bottom: 0;
-					background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
+					// background: linear-gradient(135deg, #667eea 0%, #764ba2 100%);
 					display: flex;
 					align-items: center;
 					justify-content: center;
